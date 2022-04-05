934cf837dced19d2f6a34dc86ea61af976e493d6
Robert Slocumbe <rslocumbe93@gmail.com>
2022-04-04 21:45:31 -0400

    added nav and search HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var APIKey = '9ee21022229bd5d692c985f06baf6a14';
 var city = 'Portland';
 var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+var test = document.getElementById("test");
 
 fetch(weatherUrl)
     .then(function (response) {
@@ -8,7 +9,6 @@ fetch(weatherUrl)
     })
     .then(function (data) {
         console.log(data);
-        console.log(data.clouds);
+        console.log(data.weather);
+        test.textContent = data.weather[0].icon
     });
-
-console.log(data.clouds);
